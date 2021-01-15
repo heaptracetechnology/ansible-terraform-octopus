04f29e2c1c82b45e812f4aef4a6fcbefaab367a6
ATUL <62421988+Atul-devops@users.noreply.github.com>
2021-01-15 17:46:00 +0530

    Create variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,6 +21,37 @@ variable "key_name" {
   default = "terraform-ansible-example-key"
 }
 
+resource "aws_security_group" "default" {
+  name        = "terraform_example"
+  description = "Used in the terraform"
+  vpc_id      = "${aws_vpc.default.id}"
+  tags = "${var.tags}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
 variable "aws-subnet-id" {}
 variable "aws-vpc-security-group-id" {}

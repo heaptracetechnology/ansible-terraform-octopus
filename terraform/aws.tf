310b0a48a84470261ee636b1504cf081a8f5ac52
ATUL <62421988+Atul-devops@users.noreply.github.com>
2021-01-15 10:58:18 +0530

    Create aws.tf

---
+++ terraform/aws.tf
@@ -0,0 +1,9 @@
+# Specify the provider and access details
+provider "aws" {
+        access_key = "access_key"
+        secret_key = "secret_key"
+        region = "region"
+
+  version = "~> 2.63"
+
+  }
